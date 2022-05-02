6b16e5c676b09a795225d56102574a06a60dc477
Samiya <skmcintosh20@gmail.com>
2022-05-02 14:23:45 -0700

    finished property list component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import "./home.css";
 import {Navbar} from "../../components/navbar/Navbar";
 import {Header} from "../../components/header/Header";
 import {Featured} from "../../components/featured/Featured";
+import {PropertyList} from "../../components/propertyList/PropertyList";
 
 export const Home = () => {
 	return (
@@ -10,6 +11,9 @@ export const Home = () => {
 			<Header />
 			<div className="home__Container">
 				<Featured />
+				<h1 className="home__Title">Browse by property type</h1>
+				<PropertyList />
+				<h1 className="home__Title">Homes guests love</h1>
 			</div>
 		</div>
 	);
