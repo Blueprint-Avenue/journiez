697be71db411db94054e3cbddc3f1b193977a964
Samiya <skmcintosh20@gmail.com>
2022-05-02 15:03:10 -0700

    completed footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,50 @@
+import "./footer.css";
+
+export const Footer = () => {
+	return (
+		<div className="footer">
+			<div className="footer__Lists">
+				<ul className="footer__List">
+					<li className="footer__ListItem">Countries</li>
+					<li className="footer__ListItem">Regions</li>
+					<li className="footer__ListItem">Cities</li>
+					<li className="footer__ListItem">Districts</li>
+					<li className="footer__ListItem">Airports</li>
+					<li className="footer__ListItem">Hotels</li>
+				</ul>
+				<ul className="footer__List">
+					<li className="footer__ListItem">Homes </li>
+					<li className="footer__ListItem">Apartments </li>
+					<li className="footer__ListItem">Resorts </li>
+					<li className="footer__ListItem">Villas</li>
+					<li className="footer__ListItem">Hostels</li>
+					<li className="footer__ListItem">Guest houses</li>
+				</ul>
+				<ul className="footer__List">
+					<li className="footer__ListItem">Unique places to stay </li>
+					<li className="footer__ListItem">Reviews</li>
+					<li className="footer__ListItem">Unpacked: Travel articles </li>
+					<li className="footer__ListItem">Travel communities </li>
+					<li className="footer__ListItem">Seasonal and holiday deals </li>
+				</ul>
+				<ul className="footer__List">
+					<li className="footer__ListItem">Car rental </li>
+					<li className="footer__ListItem">Flight Finder</li>
+					<li className="footer__ListItem">Restaurant reservations </li>
+					<li className="footer__ListItem">Travel Agents </li>
+				</ul>
+				<ul className="footer__List">
+					<li className="footer__ListItem">Customer Service</li>
+					<li className="footer__ListItem">Partner Help</li>
+					<li className="footer__ListItem">Careers</li>
+					<li className="footer__ListItem">Sustainability</li>
+					<li className="footer__ListItem">Press center</li>
+					<li className="footer__ListItem">Safety Resource Center</li>
+					<li className="footer__ListItem">Investor relations</li>
+					<li className="footer__ListItem">Terms & conditions</li>
+				</ul>
+			</div>
+			<div className="footer__Text">Copyright © 2022 Journiez.</div>
+		</div>
+	);
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,7 @@ import {Featured} from "../../components/featured/Featured";
 import {PropertyList} from "../../components/propertyList/PropertyList";
 import {FeaturedProperties} from "../../components/featuredProperties/FeaturedProperties";
 import {MailList} from "../../components/mailList/MailList";
+import {Footer} from "../../components/footer/Footer";
 
 export const Home = () => {
 	return (
@@ -18,6 +19,7 @@ export const Home = () => {
 				<h1 className="home__Title">Homes guests love</h1>
 				<FeaturedProperties />
 				<MailList />
+				<Footer />
 			</div>
 		</div>
 	);
