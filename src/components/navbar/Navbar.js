d83de08b3c02d2ccc3bb92d5acc2a2813186f0e8
Samiya <skmcintosh20@gmail.com>
2022-05-02 12:05:41 -0700

    created navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import "./navbar.css";
+
+export const Navbar = () => {
+	return (
+		<div className="navbar">
+			<div className="nav__Container">
+				<span className="logo">Journiez Reserve</span>
+				<div className="nav__Items">
+					<button className="nav__button">Register</button>
+					<button className="nav__button">Login</button>
+				</div>
+			</div>
+		</div>
+	);
+};
