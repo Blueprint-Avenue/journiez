45f9840327f070aece7083ed75aa2959ff607bab
Samiya <skmcintosh20@gmail.com>
2022-05-02 11:33:59 -0700

    added folders to components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
+import "./App.css";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/hotels" element={<List />} />
-				<Route path="/hotels/:id" element={<Hotel />} />
-			</Routes>
-		</BrowserRouter>
+		<div className="App">
+			<h1>Helloooooo</h1>
+		</div>
 	);
 }
 
