12db01e15aac1dc704d302a0a1b850478d933452
Samiya <skmcintosh20@gmail.com>
2022-05-02 14:37:43 -0700

    completed featuredProperties Component and css

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import {Navbar} from "../../components/navbar/Navbar";
 import {Header} from "../../components/header/Header";
 import {Featured} from "../../components/featured/Featured";
 import {PropertyList} from "../../components/propertyList/PropertyList";
+import {FeaturedProperties} from "../../components/featuredProperties/FeaturedProperties";
 
 export const Home = () => {
 	return (
@@ -14,6 +15,7 @@ export const Home = () => {
 				<h1 className="home__Title">Browse by property type</h1>
 				<PropertyList />
 				<h1 className="home__Title">Homes guests love</h1>
+				<FeaturedProperties />
 			</div>
 		</div>
 	);
